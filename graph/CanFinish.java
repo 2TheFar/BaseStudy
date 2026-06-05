efee782a6eb37ed1f02c6ec0e6cbd9a97ef665a8
2Far <137515778+2TheFar@users.noreply.github.com>
2026-06-05 23:23:18 +0800

    图论结束

--- graph/CanFinish.java
+++ graph/CanFinish.java
@@ -1,10 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-
-import CopyRandomList.Node;
 
 public class CanFinish {
     /**
